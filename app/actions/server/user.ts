817f4f06ccfde514160ca89181b260984af5e25e
hex-aragon <0xaragon90@gmail.com>
2024-05-25 16:44:48 +0900

    ADD : prisma schema test

--- app/actions/server/user.ts
+++ app/actions/server/user.ts
@@ -1,8 +1,8 @@
-'use server'
+"use server";
 
-import { Prisma, PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+import { Prisma, PrismaClient } from "@prisma/client";
+const { faker } = require("@faker-js/faker");
+const prisma = new PrismaClient();
 
 export const getUserById = (id: string) => {
   return prisma.user.findUnique({
@@ -12,5 +12,31 @@ export const getUserById = (id: string) => {
     select: {
       name: true,
     },
-  })
+  });
+};
+
+async function main() {
+  const users = [];
+
+  for (let i = 0; i < 10; i++) {
+    users.push({
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+    });
+  }
+
+  await prisma.user.createMany({
+    data: users,
+  });
+
+  console.log("Inserted 10 users");
 }
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
